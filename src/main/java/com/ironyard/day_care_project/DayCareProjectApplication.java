5136f9593740aabb7ce1c57c9e646fe93891c845
hls93 <hlshaw3@ncsu.edu>
2017-09-25 16:02:20 -0400

    initializing repo

---
+++ src/main/java/com/ironyard/day_care_project/DayCareProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ironyard.day_care_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayCareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayCareProjectApplication.class, args);
+	}
+}
