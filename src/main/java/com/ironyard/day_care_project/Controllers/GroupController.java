b4927dad85929166e07e2cca6f86051b8c95f18c
Cindi Harris <cmr0319@gmail.com>
2017-10-05 15:32:14 -0400

    made changes to group controller

--- src/main/java/com/ironyard/day_care_project/Controllers/GroupController.java
+++ src/main/java/com/ironyard/day_care_project/Controllers/GroupController.java
@@ -3,6 +3,7 @@ package com.ironyard.day_care_project.Controllers;
 
 import com.ironyard.day_care_project.Entity.Group;
 import com.ironyard.day_care_project.Repos.DaycareRepository;
+import com.ironyard.day_care_project.Repos.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,12 @@ import java.util.List;
 public class GroupController {
 
     @Autowired
-    private DaycareRepository daycareRepo;
+    private GroupRepository groupRepo;
 
     @GetMapping("/daycares/groups")
     public List<Group> daycareList(){
 
-        return daycareRepo.findAll();
+        return groupRepo.findAll();
     }
 
 }
---
+++ src/main/java/com/ironyard/day_care_project/Repos/GroupRepository.java
@@ -0,0 +1,7 @@
+package com.ironyard.day_care_project.Repos;
+
+import com.ironyard.day_care_project.Entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository <Group, Integer> {
+}
