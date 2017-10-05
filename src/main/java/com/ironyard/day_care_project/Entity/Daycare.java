c6b89b6be0f4c48e03d934f362748582b11d7474
hls93 <hlshaw3@ncsu.edu>
2017-10-05 14:19:53 -0400

    changing entity names

--- src/main/java/com/ironyard/day_care_project/Entity/Daycare.java
+++ src/main/java/com/ironyard/day_care_project/Entity/Daycare.java
@@ -5,12 +5,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
-public class User {
+public class Daycare {
 
     @Id
     @GeneratedValue
     private Integer id;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String zipcode;
 
     private String name;
 
@@ -62,5 +69,37 @@ public class User {
 
     public void setGroups(Set<Group> groups) {
         this.groups = groups;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 }
---
+++ src/main/java/com/ironyard/day_care_project/Repos/DaycareRepository.java
@@ -0,0 +1,8 @@
+package com.ironyard.day_care_project.Repos;
+
+import com.ironyard.day_care_project.Entity.Daycare;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DaycareRepository extends JpaRepository<Daycare, Integer>  {
+
+}
