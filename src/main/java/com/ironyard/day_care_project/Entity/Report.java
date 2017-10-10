aaeb76bebefb340351e8e5ef74aeaef8faba175c
hls93 <hlshaw3@ncsu.edu>
2017-10-10 10:41:51 -0400

    report repos and controller

--- src/main/java/com/ironyard/day_care_project/Entity/Report.java
+++ src/main/java/com/ironyard/day_care_project/Entity/Report.java
@@ -12,10 +12,56 @@ public class Report {
     private ArrayList<Nap> naps;
     private String note;
 
+    public Report() {
+    }
 
+    public Report(ArrayList<DiaperChange> diaperChangee, ArrayList<ItemNeeded> itemsNeeded, ArrayList<Meal> meals, ArrayList<Nap> naps, String note) {
+        this.diaperChangee = diaperChangee;
+        this.itemsNeeded = itemsNeeded;
+        this.meals = meals;
+        this.naps = naps;
+        this.note = note;
+    }
+
+    public ArrayList<DiaperChange> getDiaperChangee() {
+        return diaperChangee;
+    }
+
+    public void setDiaperChangee(ArrayList<DiaperChange> diaperChangee) {
+        this.diaperChangee = diaperChangee;
+    }
+
+    public ArrayList<ItemNeeded> getItemsNeeded() {
+        return itemsNeeded;
+    }
+
+    public void setItemsNeeded(ArrayList<ItemNeeded> itemsNeeded) {
+        this.itemsNeeded = itemsNeeded;
+    }
+
+    public ArrayList<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(ArrayList<Meal> meals) {
+        this.meals = meals;
+    }
+
+    public ArrayList<Nap> getNaps() {
+        return naps;
+    }
+
+    public void setNaps(ArrayList<Nap> naps) {
+        this.naps = naps;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
 
-//        itemsNeeded: {wipes: true, diapers: false, clothes: true},
-//        meals: [{type: "breakfast", food: "Bacon, Eggs, Toast", amount: "most"},{type: "lunch", food: "Sandwich", amount: "all"}],
-//        naps: [{napStart: "1130", napEnd: "1230"}],
-//        note: 'Billy had a really great day!'
+
