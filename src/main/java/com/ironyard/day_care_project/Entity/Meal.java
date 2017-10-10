45bf50520226ace48646e27ca57b1f8c0724090a
Cindi Harris <cmr0319@gmail.com>
2017-10-10 10:46:12 -0400

    added properties to meal and nap entities as well as empty constructors and getters/setters

--- src/main/java/com/ironyard/day_care_project/Entity/Meal.java
+++ src/main/java/com/ironyard/day_care_project/Entity/Meal.java
@@ -4,4 +4,35 @@ import javax.persistence.Entity;
 
 @Entity
 public class Meal {
+
+    private String type;
+    private String food;
+    private String amount;
+
+    public Meal() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/com/ironyard/day_care_project/Entity/Nap.java
+++ src/main/java/com/ironyard/day_care_project/Entity/Nap.java
@@ -4,4 +4,27 @@ import javax.persistence.Entity;
 
 @Entity
 public class Nap {
+    private Integer napStart;
+    private Integer napEnd;
+
+    public Nap() {
+    }
+
+    public Integer getNapStart() {
+        return napStart;
+    }
+
+    public void setNapStart(Integer napStart) {
+        this.napStart = napStart;
+    }
+
+    public Integer getNapEnd() {
+        return napEnd;
+    }
+
+    public void setNapEnd(Integer napEnd) {
+        this.napEnd = napEnd;
+    }
+
+
 }
